f6431216c8082fd4349be0641daaae413ba096c8
Alexandre Rademaker <arademaker@gmail.com>
2020-02-07 16:22:42 -0300

    new segmentations of opennlp

---
+++ opennlp/split.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ONLP=/Users/ar/work/apache-opennlp-1.9.0
+
+T1=$(basename $1 .raw).tmp1
+T2=$(basename $1 .raw).tmp2
+R=$(basename $1 .raw).sent
+
+awk 'NF {printf "%s " ,$0 ; next} {print ""}' $1 > $T1
+$ONLP/bin/opennlp SentenceDetector $ONLP/models/pt-sent.bin < $T1 > $T2
+awk '$0 ~ /^$/ {next} {print}' $T2 > $R
+gsed -i ':a;N;$!ba;s/S\.\nPaulo/S\. Paulo/g' $R
+gsed -E -i ':a;N;$!ba;s/([Ss])r\.\n/\1r\. /g' $R
+gsed -i 's/ \+/ /g' $R
+rm $T1 $T2
