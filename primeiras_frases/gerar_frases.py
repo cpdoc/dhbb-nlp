171df332dea6de582177c27c48ee17acdaf8b18c
Cristiananc <cristiananc@hotmail.com>
2020-04-02 20:56:31 -0300

    gerar frases

--- primeiras_frases/gerar_frases.py
+++ primeiras_frases/gerar_frases.py
@@ -18,7 +18,7 @@ def checar_biografia(s):
     input: string, texto do DHBB
     output: True ou False
     '''
-    s = s.replace("\n\n",'__TEMP__').replace("\n",' ').replace('__TEMP__',' ').split()
+
     if "«" in s[0]:
         return True
     return False
@@ -31,8 +31,6 @@ def count(s,char):
     return i
 
 
-
-
 if __name__ == '__main__':
     
     try:
@@ -40,22 +38,20 @@ if __name__ == '__main__':
         if dir_[-1] != "/":
             dir_ += "/"
     except IndexError:
-        print("Necessita do path do diretório raw como argumento. Exemplo: python3 gerar_frases.py path/to/raw")
+        print("Necessita do path do diretório raw como argumento. Exemplo: python3 gerar_frases.py path/to/fl")
         exit()
-    files = [t for t in os.listdir(dir_) if 'raw' in t]
+    files = [t for t in os.listdir(dir_) if 'sent' in t]
     sent = []
     shuffle(files)
     i = 0
     log = ''
     for file in files:
-        text = open(dir_+file.replace('text','raw'),'r', encoding = 'utf-8').read()
-        text_1 = text.replace("\n\n",'__TEMP__').replace('\n',' ')
-        sent_text = text_1.split('__TEMP__')
+        text = open(dir_+file.replace('text','sent'),'r', encoding = 'utf-8').read()
+
+        sent_text = text.split('\n')
         if checar_biografia(text):
-            if count(sent_text[0][1:],'.') == 1:
-                if words(sent_text[0][1:]) >= 15:
-                    log += sent_text[0][1:] + '\n'
-                    i+=1
+           log += sent_text[0][1:] + '\n'
+           i+=1
         if i >= 250:
             log = log[:-1]
             break
