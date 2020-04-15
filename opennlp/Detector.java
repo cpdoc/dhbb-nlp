f01628314493e76da52bcc4a8e238b582af9884f
Alexandre Rademaker <arademaker@gmail.com>
2020-04-15 11:09:57 -0300

    adding parameters java code

--- opennlp/Detector.java
+++ opennlp/Detector.java
@@ -8,20 +8,25 @@ import java.nio.charset.*;
 class Detector {
     public static void main( String []args ) {
 
-	try (FileInputStream modelIn = new FileInputStream("/Users/ar/work/apache-opennlp-1.9.2/models/pt-sent.bin")) {
-	    SentenceModel model = new SentenceModel(modelIn);
+	try (FileInputStream modelIn = new FileInputStream(args[0])) {
 
+	    System.out.println("model: [" + args[0] + "]");
+	    System.out.println("Directory: [" + args[1] + "]");
+	    System.out.println("Extension output: [" + args[2] + "]");
+	    System.out.println("Extension  input: [" + args[3] + "]");
+	    
+	    SentenceModel model = new SentenceModel(modelIn);
 	    SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
 
-	    File[] files = new File(args[0]).listFiles();
+	    File[] files = new File(args[1]).listFiles();
 	    for (File file : files) {
-		if(file.getName().endsWith(".tmp")){
+		if(file.getName().endsWith(args[2])){
 		    System.out.println("Reading " + file.getName()); 
 			
 		    String text = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
 		    String sentences[] = sentenceDetector.sentDetect(text);
 
-		    String fnout = file.getName().replace(".tmp",".sent");
+		    String fnout = file.getName().replace(args[2],args[3]);
 		    System.out.println("Writing " + fnout); 
 		    File fout = new File(fnout);
 		    FileOutputStream fos = new FileOutputStream(fout);
@@ -34,13 +39,9 @@ class Detector {
 		    bw.close();
 		}
 	    }
-	    // le um dado diretorio
-	    // loop pelos arquivos .raw deste diretorio
-	    // chamando o sentDetect para cada file
-	    
 
 	} catch(Exception e) {
-	    System.out.println("Erro");
+	    System.out.println("Erro!");
 	}
     }
 }
--- opennlp/make.sh
+++ opennlp/make.sh
@@ -6,15 +6,15 @@ for f in ../raw/*.raw; do
 done
 
 echo run java...
-java -cp "/Users/ar/work/apache-opennlp-1.9.2/lib/*:." Detector .
+java -cp "/Users/ar/work/apache-opennlp-1.9.2/lib/*:." Detector /Users/ar/work/apache-opennlp-1.9.2/models/pt-sent.bin . .tmp .out
 
 echo post processing...
-for f in *.sent; do
-    R=$(basename $f .sent).tmp
+for f in *.out; do
+    R=$(basename $f .out).sent
     awk '$0 ~ /^$/ {next} {print}' $f > $R
     gsed -i ':a;N;$!ba;s/S\.\nPaulo/S\. Paulo/g' $R
     gsed -E -i ':a;N;$!ba;s/ ([SsdD]ra?)\.\n/ \1\. /g' $R
     gsed -E -i ':a;N;$!ba;s/ ([A-Z])\.\n([A-Z])/ \1\. \2/g' $R
     gsed -i 's/ \+/ /g' $R
-    mv $R $f
+    rm $f $(basename $f .out).tmp
 done
