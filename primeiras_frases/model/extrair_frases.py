af15ab6ab056ecaf67cc117068d516250e4d0434
lucasrct <lucasribeirocoutinho@gmail.com>
2020-04-25 11:47:40 -0300

    adicionando nome do arquivo nas sentenças

--- primeiras_frases/model/extrair_frases.py
+++ primeiras_frases/model/extrair_frases.py
@@ -1,5 +1,6 @@
 from conllu import parse
 from sys import argv,exit, stderr
+from random import shuffle
 
 
 """
@@ -15,13 +16,18 @@ frases = parse(data)
 com_status = [frase for frase in frases if 'status' in frase.metadata.keys()]
 
 revisado = [frase for frase in com_status if frase.metadata['status'] == 'revisado']
+shuffle(revisado)
 
 len_treino = round(len(revisado)*0.65,0)
-len_teste = len(revisado) - len_treino 
+len_teste_dev = len(revisado) - len_treino
+len_teste = round(0.5*len_teste_dev,0)
+len_dev = len_teste_dev - len_teste
+print("Dividinto dataset em:\nTreino: {} frases \t frases_treino.conllu\nTeste: {} frases \t frases_teste.sent\nDev: {} frases \t frases_dev.conllu\nTotal: {} frases\nCriado arquivo frases_target.conllu as frases de teste em formato gold.".format(len_treino,len_teste,len_dev,len(revisado)))
 
 conllu = ''
 i = 0
 target = ''
+dev = ''
 
 for x in revisado:
     conllu+=x.serialize()
@@ -32,13 +38,17 @@ for x in revisado:
 
 t = ''
 while i < len(revisado):
-    t+=revisado[i].metadata['text']
-    target += revisado[i].serialize()
-    target+='\n'
-    t+= '\n'
+    if i < len(revisado) - len_dev:
+        t+=revisado[i].metadata['text']
+        target += revisado[i].serialize()
+        target+='\n'
+        t+= '\n'
+    else:
+        dev+=revisado[i].serialize()
+        dev += '\n'
     i+=1
-t = t
-conllu = conllu
+
+
 
 with open('frases_treino.conllu','w') as arq:
     arq.write(conllu)
@@ -48,6 +58,10 @@ with open('frases_teste.sent','w') as arq:
         
 with open('frases_target.conllu','w') as arq:
     arq.write(target)
-    arq.close()
+    
+with open('frases_dev.conllu','w') as arq:
+    arq.write(dev)
+
+
 
     
