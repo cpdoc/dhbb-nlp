12176798d828155df3686458d03f73ad9dbd4309
Alexandre Rademaker <arademaker@gmail.com>
2020-04-28 11:25:53 -0300

    scripts compare morphobr

--- udp/not_in_morpho.sh
+++ udp/not_in_morpho.sh
@@ -1,21 +1,14 @@
-MORPHO_DIR=~/work/MorphoBr/verbs
+MORPHO_DIR=~/work/morpho-br/verbs
 
-#Gera um arquivo ordenado verbs.verb contendo todos os lemas de verbos que ocorrem nos arquivos .conllu
-awk '$4 ~ /VERB/ {print tolower($3)}' *.conllu > verbs.verb
-sort verbs.verb > verbs.temp
-uniq verbs.temp > verbs.verb
+# gera um arquivo ordenado verbs.verb contendo todos os lemas de
+# verbos que ocorrem nos arquivos .conllu
+awk '$4 ~ /VERB/ {print $3}' *.conllu | sort | uniq -c | awk '{print $2,$1}' | sort > verbs.dhbb
 
-#Extrai os verbos do morphoBr no formato infinitivo e cria um arquivo morpho.verb com os verbos ordenados
-awk 'gsub("[+A-Z0-9]","",$2) {print $2}' $MORPHO_DIR/*.dict > morpho.verb
-sort morpho.verb -o morpho.verb
-uniq morpho.verb > morpho.temp
-mv morpho.temp morpho.verb
+# extrai os verbos do morphoBr no formato infinitivo e cria um arquivo
+# morpho.verb com os verbos ordenados
+awk '{split($2,a,/\+/); print a[1]}' $MORPHO_DIR/*.dict | sort | uniq > verbs.morpho
 
-#Cria um arquivo com os lemas que estão em verbs.verb e não estão no morpho.verb 
-comm -31 morpho.verb verbs.verb > not_verbs.verb
+# cria um arquivo com os lemas que estão em verbs.verb e não estão no
+# morpho.verb
+comm -31 verbs.morpho verbs.dhbb > verbs.not_in_morpho
 
-#Cria um arquivo contendo a frequência dos verbos que não estão no morphoBr
-grep -F -x -v -f morpho.verb verbs.temp > not_verbs.tmp
-uniq -c not_verbs.tmp | sort -nr > not_verbs.count
-
-rm verbs.temp not_verbs.tmp
