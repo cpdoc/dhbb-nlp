c73432f32c554910781e474e53ed8f0deb95079b
Alexandre Rademaker <arademaker@gmail.com>
2020-04-28 12:15:36 -0300

    fix format files tsv

--- udp/not_in_morpho.sh
+++ udp/not_in_morpho.sh
@@ -2,18 +2,18 @@ MORPHO_DIR=~/work/morpho-br/verbs
 
 # gera um arquivo ordenado verbs.verb contendo todos os lemas de
 # verbos que ocorrem nos arquivos .conllu
-awk '$4 ~ /VERB/ {print $3}' *.conllu | sort | uniq -c | awk '{print $2,$1}' | sort > verbs.dhbb
-awk '$4 ~ /VERB/ {print tolower($2),$3}' *.conllu | sort | uniq -c | awk '{print $2,$3,$1}' | sort > tverbs.dhbb
+awk '$4 ~ /VERB/ {print $3}' *.conllu | sort | uniq -c | awk 'BEGIN {OFS="\t"} {print $2,$1}' | sort > verbs.dhbb
+awk '$4 ~ /VERB/ {print tolower($2),$3}' *.conllu | sort | uniq -c | awk 'BEGIN {OFS="\t"} {print $2,$3,$1}' | sort > tverbs.dhbb
 
 # extrai os verbos do morphoBr no formato infinitivo e cria um arquivo
 # morpho.verb com os verbos ordenados
 awk '{split($2,a,/\+/); print a[1]}' $MORPHO_DIR/*.dict | sort | uniq > verbs.morpho
-awk '{split($2,a,/\+/); print $1,a[1]}' $MORPHO_DIR/*.dict | sort | uniq -c | awk '{print $2,$3,$1}' | sort  > tverbs.morpho
+awk '{split($2,a,/\+/); print $1,a[1]}' $MORPHO_DIR/*.dict | sort | uniq -c | awk 'BEGIN {OFS="\t"} {print $2,$3,$1}' | sort  > tverbs.morpho
 
 # cria um arquivo com os lemas que estão em verbs.verb e não estão no
 # morpho.verb
-join -v 1 verbs.dhbb verbs.morpho > verb-lemmas-not-morpho.tsv
+join -t \t -v 1 verbs.dhbb verbs.morpho > verbs-missing-by-lemma.tsv
 
 # now by form instead of lemma
-join -v 1 tverbs.dhbb tverbs.morpho > verb-forms-not-morpho.tsv
+join -t \t -v 1 tverbs.dhbb tverbs.morpho > verbs-missing-by-form.tsv
 rm verbs.dhbb tverbs.dhbb verbs.morpho tverbs.morpho
