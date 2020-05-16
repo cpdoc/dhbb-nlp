25f96546437e3714458fcc584fc7f305872a046e
Alexandre Rademaker <arademaker@gmail.com>
2020-05-16 20:29:55 -0300

    lisp code for call OpenNLP

--- opennlp/make.sh
+++ opennlp/make.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-echo getting files...
-for f in ../raw/*.raw; do
-    awk 'NF {printf "%s " ,$0 ; next} {print ""}' $f > $(basename $f .raw).tmp;
-done
+abcl --load detector.lisp --eval '(main)' --eval '(quit)'
 
-echo run java...
-java -cp "/Users/ar/work/apache-opennlp-1.9.2/lib/*:." Detector bosque_model.bin . .tmp .out
-
-echo post processing...
-for f in *.out; do
-    R=$(basename $f .out).sent
-    awk '$0 ~ /^$/ {next} {print}' $f > $R
-    gsed -i ':a;N;$!ba;s/S\.\nPaulo/S\. Paulo/g' $R
-    gsed -E -i ':a;N;$!ba;s/ ([SsdD]ra?)\.\n/ \1\. /g' $R
-    gsed -E -i ':a;N;$!ba;s/ ([A-Z])\.\n([A-Z])/ \1\. \2/g' $R
-    gsed -i 's/ \+/ /g' $R
-    rm $f $(basename $f .out).tmp
-done
