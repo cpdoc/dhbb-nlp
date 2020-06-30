de6742f467c29a39fd2fe0116e18e98bf46ddfb1
Alexandre Rademaker <arademaker@gmail.com>
2020-06-29 22:32:38 -0300

    finish reorganization

--- sents/detector.lisp
+++ sents/detector.lisp
@@ -16,21 +16,20 @@
 	      res)))))
 
 
-(defun offsets (file)
+(defun offsets (txt)
   (let (res)
-    (with-open-file (in file)
+    (with-input-from-string (in txt)
       (handler-case 
 	  (loop for obj = (cl-json:decode-json in)
 		do (setf res (append res (offset obj))))
 	(end-of-file () res)))))
 
 
-(defun freeling (txt fn)
-  (let ((in  (make-string-input-stream txt))
-	(jfn (make-pathname :directory nil :type "json" :defaults fn))
-	(res nil))    
-    (uiop:run-program (list "analyzer_client" "8000") :input in :output jfn :if-output-exists :supersede)
-    (dolist (o (offsets jfn)
+(defun freeling (txt)
+  (let* ((in  (make-string-input-stream txt))
+	 (out (uiop:run-program (list "analyzer_client" "8000") :input in :output :string))
+	 (res nil))
+    (dolist (o (offsets out)
 	       (make-array (length res) :initial-contents (reverse res)))
       (push (cl-ppcre:regex-replace-all "\\n+" (subseq txt (nth 0 o) (nth 1 o)) " ")
 	    res))))
@@ -44,7 +43,7 @@
     (string-trim '(#\Space) (cl-ppcre:regex-replace-all "[ ]{2,}" d1 " "))))
 
 
-(defun save-array-to-file (arr fn)
+(defun save-array (arr fn)
   (with-open-file (out fn :direction :output :if-exists :supersede)
     (dotimes (n (array-dimension arr 0))
       (format out "~a~%" (aref arr n)))))
@@ -59,17 +58,17 @@
     (dolist (fn (directory "~/work/cpdoc/dhbb-nlp/raw/*.raw"))
       (let* ((txt     (read-content fn))
 	     (sents-1 (jcall detect detector txt))
-	     (sents-2 (freeling txt fn)))
+	     (sents-2 (freeling txt)))
 	(format t "=> Processing ~a~%" fn)
 	(if (equal (array-dimension sents-1 0)
 		   (array-dimension sents-2 0))
 	    (progn
 	      (format t "=> OpenNLP and Freeling agree~%")
-	      (save-array-to-file sents-1 (make-pathname :directory nil :type "sent" :defaults fn)))
+	      (save-array sents-1 (make-pathname :directory nil :type "sent" :defaults fn)))
 	    (progn
 	      (format t "=> OpenNLP and Freeling disagree~%")
-	      (save-array-to-file sents-1 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "onlp")
-							 :directory nil :type "sent" :defaults fn))
-	      (save-array-to-file sents-2 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "fl")
-							 :directory nil :type "sent" :defaults fn))))))))
+	      (save-array sents-1 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "op")
+						 :directory nil :type "sent" :defaults fn))
+	      (save-array sents-2 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "fl")
+						 :directory nil :type "sent" :defaults fn))))))))
 
