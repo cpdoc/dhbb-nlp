22f38152528b42d9c0a0f88058ef06c69192d55a
lucasrct <lucasribeiro@gmail.com>
2020-06-30 22:23:59 -0300

    Adicionado modelo do NTLK-Punkt treinado com o DHBB

--- primeiras_frases/udpipe.sh
+++ primeiras_frases/udpipe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo $1 > tmp
-udpipe --tokenize --tokenizer="presegmented" --tag --parse $2 tmp 
+udpipe --tokenize --tokenizer="presegmented" --tag --parse ../udp/model_enhanced.bin $2 tmp 
 rm tmp
