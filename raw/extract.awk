941ff195bee890d5e05f08064c05f6c056527afe
Alexandre Rademaker <arademaker@gmail.com>
2020-07-14 19:55:02 -0300

    sentence split using offset

--- raw/extract.awk
+++ raw/extract.awk
@@ -1,8 +1,42 @@
-BEGIN { text=0; }
 
-$0 ~ /^#/ { next }
+function cprint(var){
+    gsub(/\n/," ", var);
+    gsub(/[ ]+/," ",var);
+    gsub(/^[ ]*/,"",var);
+    gsub(/[ ]*$/,"",var);
+    print var;
+}    
 
-text>1 { print }
 
-/^---$/ { text = text + 1; }
+BEGIN { text=0; cpar=""; state=0; }
+
+/^---$/ && state==0 { state=1; next; }
+
+/^---$/ && state==1 { state=2; next; }
+
+$0 ~ /^#/     && state==2 { next; }
+$0 ~ /^[ ]*$/ && state==2 { next; }
+
+$0 !~ /^[ ]*$/ && state==2 {
+    cpar  = $0;
+    state = 3;
+    next;
+}
+
+$0 ~ /^[ ]*$/ && state==3 {
+    cprint(cpar);
+    state=2;
+    cpar = "";
+    next;
+}    
+
+$0 !~ /^[ ]*$/ && state==3 {
+    cpar = cpar " " $0;
+    next;
+}
+
+END {
+    if(length(cpar)>0) cprint(cpar);
+}    
+
 
--- sents/detector.lisp
+++ sents/detector.lisp
@@ -1,5 +1,6 @@
 
 ;; to be runned with ABCL Lisp
+;; useful info https://abcl.org/releases/1.7.0/abcl-1.7.0.pdf
 
 (ql:quickload '(:alexandria :cl-json :cl-ppcre))
 
@@ -30,9 +31,9 @@
 	 (out (uiop:run-program (list "analyzer_client" "8000") :input in :output :string))
 	 (res nil))
     (dolist (o (offsets out)
-	       (make-array (length res) :initial-contents (reverse res)))
-      (push (cl-ppcre:regex-replace-all "\\n+" (subseq txt (nth 0 o) (nth 1 o)) " ")
-	    res))))
+	       (make-array (list (length res) 2) :initial-contents (reverse res)))
+      (push (list (nth 0 o) (nth 1 o)) res))))
+
 
 
 (defun nltk (txt)
@@ -49,36 +50,43 @@
 ;; for regex-replace-all? we can pre-compile the regular expressions
 ;; too.
 (defun read-content (fn)
-  (let* ((d1 (cl-ppcre:regex-replace-all "\\n" (alexandria:read-file-into-string fn) " ")))
-    (string-trim '(#\Space) (cl-ppcre:regex-replace-all "[ ]{2,}" d1 " "))))
-
+  (alexandria:read-file-into-string fn))
 
 (defun save-array (arr fn)
   (with-open-file (out fn :direction :output :if-exists :supersede)
     (dotimes (n (array-dimension arr 0))
-      (format out "~a~%" (aref arr n)))))
+      (format out "~a ~a~%" (aref arr n 0) (aref arr n 1)))))
 
+
+(defun get-2-array (spans)
+  (let* ((nsent (array-dimension spans 0))
+	 (start (jmethod "opennlp.tools.util.Span" "getStart" 0))
+	 (end   (jmethod "opennlp.tools.util.Span" "getEnd" 0))
+	 (res   (make-array (list nsent 2) :initial-element 0)))
+    (dotimes (n nsent res)
+      (setf (aref res n 0) (jcall start (aref spans n))
+	    (aref res n 1) (jcall end (aref spans n))))))
 
 
 (defun main ()
   (let* ((mfile    (jinput-stream "bosque_model.bin"))
 	 (model    (jnew "opennlp.tools.sentdetect.SentenceModel" mfile))
 	 (detector (jnew "opennlp.tools.sentdetect.SentenceDetectorME" model))
-	 (detect   (jmethod "opennlp.tools.sentdetect.SentenceDetectorME" "sentDetect" 1)))
+	 (detect   (jmethod "opennlp.tools.sentdetect.SentenceDetectorME" "sentPosDetect" 1)))
     (dolist (fn (directory "~/work/cpdoc/dhbb-nlp/raw/*.raw"))
       (let* ((txt     (read-content fn))
-	     (sents-1 (jcall detect detector txt))
-	     (sents-2 (freeling txt)))
+	     (spans-1 (get-2-array (jcall detect detector txt)))
+	     (spans-2 (freeling txt)))
 	(format t "=> Processing ~a~%" fn)
-	(if (equal (array-dimension sents-1 0)
-		   (array-dimension sents-2 0))
+	(if (equal (array-dimension spans-1 0)
+		   (array-dimension spans-2 0))
 	    (progn
 	      (format t "=> OpenNLP and Freeling agree~%")
-	      (save-array sents-1 (make-pathname :directory nil :type "sent" :defaults fn)))
+	      (save-array spans-1 (make-pathname :directory nil :type "sent" :defaults fn)))
 	    (progn
 	      (format t "=> OpenNLP and Freeling disagree~%")
-	      (save-array sents-1 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "op")
+	      (save-array spans-1 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "op")
 						 :directory nil :type "sent" :defaults fn))
-	      (save-array sents-2 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "fl")
+	      (save-array spans-2 (make-pathname :name (format nil "~a-~a" (pathname-name fn) "fl")
 						 :directory nil :type "sent" :defaults fn))))))))
 
