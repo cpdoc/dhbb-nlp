033f26281839d5c45fa38b34c392dfa8a79419ce
IgorCJ <igor.junqueira@fgv.edu.br>
2020-07-16 18:19:37 -0300

    finished segmentation.py

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,7 +1,10 @@
-import nltk, sys
+import nltk.tokenize.punkt
+import sys, pickle
 
+arq=open("model_trained.pickle","rb")
+model=pickle.load(arq)
 text = sys.stdin.read()
-sents=nltk.sent_tokenize(text)
+sents = model.sentences_from_text(text)
 for element in sents:
     pos=text.find(element)
     print(str(pos)+" "+str(pos+len(element)))
