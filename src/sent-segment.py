d247a564d7ac82f60b509a3a0edd45c9948dac01
IgorCJ <igor.junqueira@fgv.edu.br>
2020-08-12 17:07:08 -0300

    atualizaçao no sent-segment

--- src/sent-segment.py
+++ src/sent-segment.py
@@ -1,13 +1,18 @@
 #Arg1 = model_trained_file ( model_punkt.pickle )
 #Arg2 = path_raw ( ../raw/ )
-#Arg3 = path_out ( ../sent/ )
+#Arg3 = path_out ( ../sents/ )
+#Arg4 = Log (booleano 0/1)
 
 import nltk.tokenize.punkt
-import sys, pickle, os
+import sys, pickle, os, time
 
+if(int(sys.argv[4])): t0=time.time()
+if(int(sys.argv[4])): print("Carregando modelo")
 arq=open(sys.argv[1],"rb")
 model=pickle.load(arq)
+if(int(sys.argv[4])): print("Modelo carregado em: {:.3} s".format(time.time()-t0))
 
+if(int(sys.argv[4])): print("Processando raw")
 for file in os.listdir(sys.argv[2]):
 	if file.endswith(".raw"):
 		text = open(os.path.join(sys.argv[2],file), 'r').read()
@@ -15,3 +20,5 @@ for file in os.listdir(sys.argv[2]):
 		with open(os.path.join(sys.argv[3],file.replace(".raw","-nk.sent")), "w") as out_file:
 			for element in sents:
 				out_file.write(str(element[0]) + " " + str(element[1])+"\n")
+
+if(int(sys.argv[4])): print("Tempo total: {:.3} s".format(time.time()-t0))
