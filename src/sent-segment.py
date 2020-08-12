b90632b68de17b2eceffe85e8b049e8dac999491
IgorCJ <igor.junqueira@fgv.edu.br>
2020-08-12 14:32:13 -0300

    Issue #59

--- src/sent-segment.py
+++ src/sent-segment.py
@@ -1,14 +1,17 @@
+#Arg1 = model_trained_file ( model_punkt.pickle )
+#Arg2 = path_raw ( ../raw/ )
+#Arg3 = path_out ( ../sent/ )
+
 import nltk.tokenize.punkt
-import sys, pickle
+import sys, pickle, os
 
 arq=open(sys.argv[1],"rb")
-# arq=open("model_punkt.pickle","rb")
 model=pickle.load(arq)
-model._params.abbrev_types.add("S.A")
-model._params.abbrev_types.add("s.a")
-text = sys.stdin.read()
 
-sents = model.span_tokenize(text)
-
-for element in sents:
-	print(str(element[0]) + " " + str(element[1]))
+for file in os.listdir(sys.argv[2]):
+	if file.endswith(".raw"):
+		text = open(os.path.join(sys.argv[2],file), 'r').read()
+		sents = model.span_tokenize(text)
+		with open(os.path.join(sys.argv[3],file.replace(".raw","-nk.sent")), "w") as out_file:
+			for element in sents:
+				out_file.write(str(element[0]) + " " + str(element[1])+"\n")
