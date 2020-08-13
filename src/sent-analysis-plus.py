acdb7f10607f2137400674642888c9cb18fa37fc
IgorCJ <igor.junqueira@fgv.edu.br>
2020-08-13 18:29:34 -0300

    implementado #60

--- src/sent-analysis-plus.py
+++ src/sent-analysis-plus.py
@@ -1,7 +1,9 @@
-import sys, re, argparse
+import sys, re, argparse, os, time
 
 parser = argparse.ArgumentParser()
-parser.add_argument('files', metavar='f', nargs='+', help='files *-??.sent to generate .sent and, if necessary, .diff')
+parser.add_argument("-i","--infile",help="single file processing mode. Only files with given start will be processed.",nargs = "?")
+parser.add_argument("-v","--verbose",help="verbose mode, prints in the stdout.", action='store_true')
+parser.add_argument('pathin', help='path to the input files')
 parser.add_argument('pathout', help='path to the output files')
 args = parser.parse_args()
 
@@ -47,24 +49,49 @@ def classify(U,union):
 def getnum(*files):
     return(files[0][:-8])
 
+def find_files(path,name):
+    l=[]
+    for filename in os.listdir(path):
+        if re.findall("^"+name+"-", filename): l.append(path+filename)
+    return l
+
+
 def record_shared(shared, path):
     with open(path,'w') as f:
         for tuples in shared:
             f.write(str(tuples[0])+' '+str(tuples[1])+'\n')
-
 
 def record_different(different, path):
     with open(path,'w') as f:
         for tuples in different:
             f.write(str(tuples[0])+' '+str(tuples[1])+' '+tuples[2]+'\n')
 
-
-
+def analysis(*files,pathout):
+    U , union = universe_generator(*files)
+    shared, different = classify(U,union)
+    num = getnum(*files)
+    record_shared(shared, pathout+num+'.sent')
+    if different: record_different(different, pathout+num+'.diff')
+    return
 
 if __name__ == '__main__':
 
-    U , union = universe_generator(*args.files)
-    shared, different = classify(U,union)
-    num = getnum(*args.files)
-    record_shared(shared, args.pathout+num+'.sent')
-    if different: record_different(different, args.pathout+num+'.diff')
+    
+    if args.infile:
+        t0=time.time()
+        if args.verbose: print("Processando arquivos.")
+        analysis(*find_files(args.pathin,args.infile),pathout=args.pathout)
+        if args.verbose: print("Processamento finalizado em {:.3} s".format(time.time()-t0))
+
+    else:
+        t0=time.time()
+        if args.verbose: print("Coletando arquivos.")
+        l=[]
+        for filename in os.listdir(args.pathin):
+            if(re.search(r'-', filename)): l.append(filename[:-8])
+        l = (list(set(l)))
+        if args.verbose: print("Processando arquivos:")
+        for element in l:
+            if args.verbose: print("Processando {:.3}-??.sent".format(element))
+            analysis(*find_files(args.pathin,element),pathout=args.pathout)
+        if args.verbose: print("Finalizado em {:.3} s".format(time.time()-t0))
