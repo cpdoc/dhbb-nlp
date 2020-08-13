9907313e0532222875f52972ecbc6c567652f27d
Alexandre Rademaker <arademaker@gmail.com>
2020-08-13 16:30:47 -0300

    related to issue #59

--- src/sent-segment.py
+++ src/sent-segment.py
@@ -1,35 +1,60 @@
 import nltk.tokenize.punkt
 import sys, pickle, os, time, argparse
+import glob
+
+# construcao argumentos programa
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-m","--model",help="model trained file path")
-parser.add_argument("indir",help="file or directory of raw to be processed")
-parser.add_argument("outdir", help="file or directory of processed sents")
-parser.add_argument("-i","--infile",help="unique file processing mode", type=bool, const=True, nargs='?')
-parser.add_argument("-s","--status",help="show status on stdout during running", type=bool, const=True, nargs='?')
-args = parser.parse_args()
+parser.add_argument("-m","--model",help="file with the model to be loaded.")
 
-t0=time.time()
-if args.status:	print("Carregando modelo.")
-if args.model: arq=open(args.model,"rb")
-else: arq=open("model_punkt.pickle","rb")
-model=pickle.load(arq)
-if args.status: print("Modelo carregado em: {:.3} s".format(time.time()-t0))
+parser.add_argument("indir",help="directory where the raw files are located.")
+
+parser.add_argument("outdir", help="directory to save the outputs.")
+
+parser.add_argument("-i","--infile",help="single file processing mode. Only this file will be processed.",
+                    nargs = "?")
+
+parser.add_argument("-v","--verbose",help="verbose mode, prints in the stdout.",
+                    action='store_true')
+
+
+# variaveis globais
+
+args = parser.parse_args()
+model = None
+t0 = time.time()
+
+if args.verbose:
+        print("Carregando modelo %s" % args.model)
+if args.model:
+        arq = open(args.model,"rb")
+        model = pickle.load(arq)
+else:
+        arq=open("model_punkt.pickle","rb")
+
+if args.verbose:
+        print("Modelo carregado em: {:.3}s".format(time.time()-t0))
 
 if args.infile:
-	text = open(args.indir, 'r').read()
-	sents = model.span_tokenize(text)
-	with open(args.outdir,'w') as out_file:
-		for element in sents:
-			out_file.write(str(element[0]) + " " + str(element[1])+"\n")
-			
+        fin = os.path.join(args.indir, args.infile)
+        text  = open(fin, 'r').read()
+        sents = model.span_tokenize(text)
+        fnout = os.path.join(args.outdir,
+                             args.infile.replace(".raw","-nk.sent"))
+        with open(fnout,'w') as out_file:
+                for element in sents:
+                        out_file.write(str(element[0]) + " " + str(element[1])+"\n")
+        if args.verbose: print("processed %s => %s" % (fin, fnout))
+
 else:
-	if args.status: print("Processando raw")
-	for file in os.listdir(args.indir):
-		if file.endswith(".raw"):
-			text = open(os.path.join(args.indir,file), 'r').read()
-			sents = model.span_tokenize(text)
-			with open(os.path.join(args.outdir,file.replace(".raw","-nk.sent")), "w") as out_file:
-				for element in sents:
-					out_file.write(str(element[0]) + " " + str(element[1])+"\n")
-	if args.status: print("Tempo total: {:.3} s".format(time.time()-t0))
+        for fn in glob.glob(os.path.join(args.indir,"*.raw")):
+                text = open(fn, 'r').read()
+                sents = model.span_tokenize(text)
+                fnout = os.path.join(args.outdir, os.path.basename(fn).replace(".raw","-nk.sent"))
+                with open(os.path.join(args.outdir,fnout), "w") as out:
+                        for element in sents:
+                                out.write(str(element[0]) + " " + str(element[1])+"\n")
+                if args.verbose: print("processed %s => %s" % (fn,fnout))
+
+if args.verbose:
+        print("Tempo total: {:.3}s".format(time.time()-t0))
--- src/sent-segment.sh
+++ src/sent-segment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SOURCE="${BASH_SOURCE[0]}"
-abcl --batch --load $(dirname $SOURCE)/sent-detector.lisp --eval '(main)' --eval '(quit)'
+abcl --batch --load $(dirname $SOURCE)/sent-segment.abcl --eval '(main)' --eval '(quit)'
