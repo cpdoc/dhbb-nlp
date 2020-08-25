eb1c5b9274a0f85cec6fd4076030381390f6ab0c
IgorCJ <igor.junqueira@fgv.edu.br>
2020-08-25 16:40:37 -0300

    funcionando com resultado em STDOUT - #60

--- src/sent-analysis.hs
+++ src/sent-analysis.hs
@@ -1,14 +1,11 @@
-
--- import Data.List
--- import System.FilePath.Posix
-
-
+import Data.List
 import System.Environment
+import Text.Printf
 
 --Configurações do DataType a ser ultilizado
 data Sent = Sent { begin::Int,
-                     end::Int,
-                     tool::[String]} deriving (Show)
+                   end::Int,
+                   tool::[String]} deriving (Show)
 
 instance Eq Sent where
     x == y = and (begin x == begin y,end x == end y)
@@ -29,6 +26,14 @@ merge s (x:xs) | s<x = s:x:xs
                | s==x = (Sent (begin x) (end x) (tool x ++ tool s)):xs
                | otherwise = x:merge s xs
 
+intercept :: [Sent] -> Int -> [Sent]
+intercept [] _ = []
+intercept (x:xs) tam = if length(tool x)==tam then (Sent (begin x) (end x) []):intercept xs tam else intercept xs tam
+
+diff :: [Sent] -> Int -> [Sent]
+diff [] _ = []
+diff (x:xs) tam = if length(tool x)<tam then x:diff xs tam else diff xs tam
+
 --Funções auxiliares de conversão de arquivos
 remove :: String -> Char -> String
 remove [] _ = []
@@ -39,7 +44,7 @@ takename :: String -> String
 takename str = tool where
     x = remove str '.'
     tool = reverse (remove (reverse x) '-')
-                
+
 file2sent :: String -> String -> [Sent]
 file2sent str ext = lista where
     leitura = [[read e :: Int |e <- words l] |l <- lines str]
@@ -48,6 +53,12 @@ file2sent str ext = lista where
 sentGenerator :: [String] -> [String] -> [Sent]
 sentGenerator [] _ = []
 sentGenerator (x:xs) (y:ys) = (file2sent x y) ++ (sentGenerator xs ys)
+
+--Função de exibição da lista de Sent em tuplas
+imprime :: [Sent] -> IO ()
+imprime (x:xs) = do
+    putStrLn (show (begin x)++" "++show (end x)++" "++(intercalate " " (tool x)))
+    if xs/=[] then imprime xs else return()
 
 --Chamar simplifica pra lista de Sent para gerar resultado final (usa as auxiliares)
 simplifica :: [Sent] -> [Sent]
@@ -60,4 +71,11 @@ main = do
     args <- getArgs
     lista <- mapM readFile args
     let tools = [takename x | x <- args]
-    print $ simplifica (sentGenerator lista tools)
+    let toolnum = length tools
+    let universe = simplifica (sentGenerator lista tools)
+    let shared = intercept universe toolnum
+    let different = diff universe toolnum
+    putStrLn "SENT:\n"
+    imprime shared
+    putStrLn "\nDIFF:\n"
+    imprime different
