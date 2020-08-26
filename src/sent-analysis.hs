01dec8b2dc0507a97721ac4443d33a3ee4ce71e1
Alexandre Rademaker <arademaker@gmail.com>
2020-08-25 21:23:22 -0300

    cleanup sentences

--- src/sent-analysis.hs
+++ src/sent-analysis.hs
@@ -1,15 +1,18 @@
---Após compilado, chamar como ./sent-analysis file1 file2 file3...
---Ele vai gerar os arquivos '.sent' e, se houver, '.diff' no diretório do file1
+-- após compilado, chamar como ./sent-analysis file1 file2 file3...
+-- ele vai gerar os arquivos '.sent' e, se houver, '.diff' no
+-- diretório do file1
 
 import Data.List
 import System.Environment
 import Text.Printf
 import System.IO
+import System.FilePath.Posix
 
 --Configurações do DataType a ser ultilizado
-data Sent = Sent { begin::Int,
-                   end::Int,
-                   tool::[String]} deriving (Show)
+
+data Sent = Sent { begin :: Int,
+                   end   :: Int,
+                   tool  :: [String]} deriving (Show)
 
 instance Eq Sent where
     x == y = and (begin x == begin y,end x == end y)
@@ -18,7 +21,9 @@ instance Ord Sent where
     compare x y = compare ((begin x,end x)) ((begin y,end y))
     (<=) x y = or [begin x <= begin y,and (begin x == begin y, end x <= end y)]
 
+
 --Funções auxiliares de processamento do DataType
+
 ordena :: Sent -> [Sent] -> [Sent]
 ordena n [] = [n]
 ordena n (x:xs) | n<x = n:x:xs
@@ -30,24 +35,26 @@ merge s (x:xs) | s<x = s:x:xs
                | s==x = (Sent (begin x) (end x) (tool x ++ tool s)):xs
                | otherwise = x:merge s xs
 
+
 intercept :: [Sent] -> Int -> [Sent]
 intercept [] _ = []
-intercept (x:xs) tam = if length(tool x)==tam then (Sent (begin x) (end x) []):intercept xs tam else intercept xs tam
+intercept (x:xs) tam = if length(tool x) == tam then
+  (Sent (begin x) (end x) []) : intercept xs tam else intercept xs tam
+
 
 diff :: [Sent] -> Int -> [Sent]
 diff [] _ = []
 diff (x:xs) tam = if length(tool x)<tam then x:diff xs tam else diff xs tam
 
---Funções auxiliares de conversão de arquivos
-remove :: String -> Char -> String
-remove [] _ = []
-remove (x:xs) c | x==c = ""
-                | otherwise = x:remove xs c
 
-takename :: String -> String
-takename str = tool where
-    x = remove str '.'
-    tool = reverse (remove (reverse x) '-')
+--Funções auxiliares de conversão de arquivos
+
+split :: Char -> String -> [String]
+split c xs = case break (==c) xs of 
+  (ls, "") -> [ls]
+  (ls, x:rs) -> ls : split c rs
+  
+takename fn = (split '-' (takeBaseName fn))!!1
 
 file2sent :: String -> String -> [Sent]
 file2sent str ext = lista where
@@ -58,28 +65,36 @@ sentGenerator :: [String] -> [String] -> [Sent]
 sentGenerator [] _ = []
 sentGenerator (x:xs) (y:ys) = (file2sent x y) ++ (sentGenerator xs ys)
 
---Função de exibição da lista de Sent em tuplas
+-- função de exibição da lista de Sent em tuplas
 imprime :: [Sent] -> [String]
 imprime [] = []
 imprime (x:xs) = str where
     str = [(show (begin x)++" "++show (end x)++" "++(intercalate " " (tool x)))] ++ (imprime xs)
     
 
---Chamar simplifica pra lista de Sent para gerar resultado final (usa as auxiliares)
+-- chamar simplifica pra lista de Sent para gerar resultado final (usa as auxiliares)
+
 simplifica :: [Sent] -> [Sent]
 simplifica l = foldr merge [] $ foldr ordena [] l
 
---Função Main. Chamar código com nome dos arquivos como parâmetros
+-- chamar código com nome dos arquivos como parâmetros
+
+save :: [Char] -> [Sent] -> [Sent] -> IO()
+save name shared [] =
+  writeFile (name ++ ".sent") (intercalate "\n" (imprime shared))
+save name shared diff =
+  do
+    writeFile (name ++ ".sent") (intercalate "\n" (imprime shared))
+    writeFile (name ++ ".diff") (intercalate "\n" (imprime diff))
+
 main :: IO ()
 main = do
-
-    args <- getArgs
-    lista <- mapM readFile args
-    let tools = [takename x | x <- args]
-    let toolnum = length tools
-    let name = remove (args!!0) '-' 
-    let universe = simplifica (sentGenerator lista tools)
-    let shared = intercept universe toolnum
-    let different = diff universe toolnum
-    writeFile (name++".sent") (intercalate "\n" (imprime shared))
-    if different /= [] then writeFile (name++".diff") (intercalate "\n" (imprime different)) else return()
+  args  <- getArgs
+  lista <- mapM readFile args
+  let tools     = [takename x | x <- args]
+      toolnum   = length tools
+      name      = (split '-' $ takeBaseName (args!!0))!!0
+      universe  = simplifica (sentGenerator lista tools)
+      shared    = intercept universe toolnum
+      different = diff universe toolnum in
+    save name shared different
--- src/sent-analysis.py
+++ src/sent-analysis.py
@@ -1,58 +1,87 @@
+import sys, re, argparse, os, time
 
-# recebe como argumento o número do arquivo (apenas número) e
-# transforma o conjunto numero-??.sent em um arquivo numero.sent e,
-# caso haja divergência, um numero.diff
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--infile",help="single file processing mode. Only files with given start will be processed.",nargs = "?")
+parser.add_argument("-v","--verbose",help="verbose mode, prints in the stdout.", action='store_true')
+parser.add_argument('pathin', help='path to the input files')
+parser.add_argument('pathout', help='path to the output files')
+args = parser.parse_args()
 
-import sys
-
-def to_list(f):
-    out=[]
+def to_tuples(f,cod):
+    l=[]
     for line in f:
-        b,e = str(line).split()
-        out.append(tuple([int(b),int(e)]))
-    return out
+        a,b = line[:-1].split()
+        l.append((int(a),int(b),cod))
+    return l  
 
-def intersection(lst1, lst2, lst3): 
-    lst4 = [value for value in lst1 if (value in lst2 and value in lst3)] 
-    return list(tuple(i) for i in lst4)
+def simplify(U):
+    t=list(set([(e[0],e[1],'') for e in U]))
+    t.sort()
+    for i in range(len(t)):
+        cor=[e for e in U if (e[0],e[1],'')==t[i]]
+        ext=[elem[2] for elem in cor]
+        ext.sort()
+        t[i]=(t[i][0],t[i][1],' '.join(ext))
+    return t
 
-def diference(f,i):
-    final_list = []
-    for line in f:
-        if not(line in i):
-            final_list.append(line)
-    return final_list
+def universe_generator(*files):
+    U=[]
+    union=[]
+    for tool in files:
+        cod = re.search('-(.*).sent', tool)
+        U = U + to_tuples(open(tool,'r').readlines(),cod.group(1))
+        union.append(cod.group(1))
+    U = simplify(U)
+    union.sort()
+    ext=' '.join(union)
+    return(U,ext)
 
-def lst_write(lst,f):
-     for element in lst:
-        f.write(str(element[0]) + " " + str(element[1])+"\n")
+def classify(U,union):
+    return([tuples for tuples in U if tuples[2]==union], [tuples for tuples in U if tuples[2]!=union])
+
+def getnum(*files):
+    return(files[0][:-8])
+
+def find_files(path,name):
+    return [path+filename for filename in os.listdir(path) if re.findall("^"+name+"-", filename)]
 
 
+def record_shared(shared, path):
+    with open(path,'w') as f:
+        for tuples in shared:
+            f.write(str(tuples[0])+' '+str(tuples[1])+'\n')
 
-number=str(sys.argv[1])
+def record_different(different, path):
+    with open(path,'w') as f:
+        for tuples in different:
+            f.write(str(tuples[0])+' '+str(tuples[1])+' '+tuples[2]+'\n')
 
-# 1. lista arquivos NNN-*.sent
-# 2. obtem os arquivos e seus sufixos: fl, nl, op...
-# 3. no diff saida, usar os sufixos do passo anterior
+def analysis(*files,pathout):
+    U , union = universe_generator(*files)
+    shared, different = classify(U,union)
+    num = getnum(*files)
+    record_shared(shared, pathout+num+'.sent')
+    if different: record_different(different, pathout+num+'.diff')
+    return
 
-fl=to_list(open(number+"-fl.sent","r").readlines())
-nl=to_list(open(number+"-nl.sent","r").readlines())
-op=to_list(open(number+"-op.sent","r").readlines())
+if __name__ == '__main__':
 
-i=intersection(fl,nl,op)
+    
+    if args.infile:
+        t0=time.time()
+        if args.verbose: print("Processando arquivos.")
+        analysis(*find_files(args.pathin,args.infile),pathout=args.pathout)
+        if args.verbose: print("Processamento finalizado em {:.3} s".format(time.time()-t0))
 
-d1=diference(fl,i)
-d2=diference(nl,i)
-d3=diference(op,i)
-
-with open(number+'.sent', 'w') as f:
-    lst_write(i,f)
-
-if (len(d1)+len(d2)+len(d3))> 0:
-    with open(str(number)+'.diff', 'w') as f:
-        f.write("#FL#\n")
-        lst_write(d1,f)
-        f.write("#NL#\n")
-        lst_write(d2,f)
-        f.write("#OP#\n")
-        lst_write(d3,f)
+    else:
+        t0=time.time()
+        if args.verbose: print("Coletando arquivos.")
+        l=[]
+        for filename in os.listdir(args.pathin):
+            if(re.search(r'-', filename)): l.append(filename[:-8])
+        l = (list(set(l)))
+        if args.verbose: print("Processando arquivos:")
+        for element in l:
+            if args.verbose: print("Processando {:.3}-??.sent".format(element))
+            analysis(*find_files(args.pathin,element),pathout=args.pathout)
+        if args.verbose: print("Finalizado em {:.3} s".format(time.time()-t0))
