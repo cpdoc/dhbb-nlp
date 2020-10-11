fc5c750cf6d1c95027002d2d076a4ebc71492416
igorcj <cj.igor@outlook.com>
2020-10-11 16:36:24 -0300

    sent-descompact working properly and adaptative to .offset and .diff

--- src/sent-descompact.hs
+++ src/sent-descompact.hs
@@ -1,17 +1,20 @@
+-- run from inside /src/ giving as argument the filepath to be processed
+
 import Data.List
 import Data.Char
 import System.Environment
 import Text.ParserCombinators.ReadP
 
--- Parser to break file into tuples like (bebin,end)
-digits :: ReadP (Int,Int) 
+-- Parser to break file into tuples like (bebin,end,tools)
+digits :: ReadP (Int,Int,String) 
 digits = do
     n1 <- munch isDigit
     munch isSpace
     n2 <- munch isDigit
-    munch (\c -> c /= '\n')
+    munch isSpace
+    t <- munch (\c -> c /= '\n')
     string "\n"
-    return (read n1,read n2)
+    return (read n1,read n2, t)
 
 -- Parser to break reversed filename into (path,name,ext) 
 filename :: ReadP (String,String,String) 
@@ -22,8 +25,11 @@ filename = do
     path <- munch (\c -> c /= ' ') 
     return (reverse path,reverse name, reverse ext)
 
-convert :: String -> (Int,Int) -> String
-convert str (b,e) = take (e-b) $ drop (b) str
+convertOffset :: String -> (Int,Int,String) -> String
+convertOffset str (b,e,_) = take (e-b) $ drop (b) str
+
+convertDiff :: String -> (Int,Int,String) -> String
+convertDiff str (b,e,t) = t++": "++(take (e-b) $ drop (b) str)
 
 main = do
     args <- getArgs
@@ -31,5 +37,5 @@ main = do
     let (path,name,ext) = fst $ head $ readP_to_S filename $ reverse $ head args
         lst = fst(last(readP_to_S  (many $ digits) content))
     raw <- readFile $ "../raw/"++name++".raw"
-    let s = [convert raw x | x <- lst]
+    let s = [f raw x | x <- lst] where f = if ext=="offset" then convertOffset else convertDiff
     writeFile (path++name++".sent") $ intercalate "\n" s
