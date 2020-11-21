d74f8e25307e67bd93a3a61debedb99ed20c7292
igorcj <cj.igor@outlook.com>
2020-11-21 14:49:46 -0300

    golden

--- src/create-golden.sh
+++ src/create-golden.sh
@@ -1,7 +1,8 @@
 # run from /src directory
-for file in ../sents/temp/*.sent. do
+for file in ../sents/temp/*.sent; do
     number=$(echo "$file" | cut -d "/" -f 4 | cut -d "." -f 1)
     git rm ../sents/$number.*
     ./sent-convert -c ../sents/temp/$number.sent ../sents/$number.goffset
     rm ../sents/temp/$number.sent
+    git add ../sents/$number.goffset
 done
