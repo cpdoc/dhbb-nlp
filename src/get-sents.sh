961a13b1a09f1f2dcf5bb667446e72f6856e4d44
igorcj <cj.igor@outlook.com>
2021-01-16 14:56:38 -0300

    OS bug solved #61

--- src/get-sents.sh
+++ src/get-sents.sh
@@ -1,6 +1,6 @@
-# Run from /src directory
+#!/bin/bash
 
-tmp=$(wc -l ../sents/*.offset | sort -n | head | awk '{print $2}')
+tmp=$(wc -l path../sents/*.offset | sort -n | head | awk '{print $2}')
 
 for file in $tmp; do
 	number=$(echo "$file" | cut -d "/" -f 3 | cut -d "." -f 1)
@@ -8,7 +8,13 @@ for file in $tmp; do
 	then
 		echo "Diff found for $number"
 		python3 sent-segment.py -m model_punkt.pickle ../raw ../sents/temp -i $number.raw
-		gsed -i s/$/' '/ ../sents/temp/$number-nk.offset
+		if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+			sed -i s/$/' '/ ../sents/temp/$number-nk.offset
+		elif [[ "$OSTYPE" == "darwin"* ]]; then
+			gsed -i s/$/' '/ ../sents/temp/$number-nk.offset
+		else
+			echo "Erro: Sistema não suportado"
+		fi
 		./sent-convert ../sents/temp/$number-nk.offset ../sents/temp/$number.sent ../raw/$number.raw
 		rm ../sents/temp/$number-nk.offset
 	else
