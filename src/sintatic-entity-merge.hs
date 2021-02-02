672c55463f88364e37ae0bd16969fa448e103240
igorcj <cj.igor@outlook.com>
2021-02-02 10:56:47 -0300

    another json aproach

--- src/sintatic-entity-merge.hs
+++ src/sintatic-entity-merge.hs
@@ -1,89 +1,143 @@
-import Control.Applicative
-import Control.Monad
-import Text.JSON
 
-data Location = Int
+-- | This JSON package retains the order of array elements.
+--   JSON: http://www.ietf.org/rfc/rfc4627.txt
 
-data Disambiguation = Disambiguation
-    { subtype :: [String]}
+module JSON (
+    JSON(..)
+  , parseJSON
+  ) where
 
-data Mention = Mention
-    { txt :: String
-    , locations :: [Location]}
+import Control.Applicative ((<*),(*>),(<$>),(<$))
+import Control.Monad (void)
+import Data.Char
+import Data.List (foldl')
+import Text.Parsec
+import Text.Parsec.String
 
-data Entity = Entity
-    { entType :: String
-    , text :: String
-    , mentions :: [Mention]
-    , disambiguation :: Disambiguation
-    , count :: Int}
+----------------------------------------------------------------
 
--- instance JSON Mention where
---     showJSON = undefined 
---     readJSON (JSObject obj) =
---         Entity            <$>
---         obj ! "text"      <*>
---         obj ! "location"
---     readJSON _ = mzero 
+parseJSON :: String -> Either ParseError JSON
+parseJSON xs = parse json "json" xs
 
+----------------------------------------------------------------
 
-instance JSON Entity where
-    showJSON = undefined 
-    readJSON (JSObject obj) =
-        Entity                 <$>
-        obj ! "type"           <*>
-        obj ! "text"           <*>
-        obj ! "mentions"       <*>
-        obj ! "disambiguation" <*>
-        obj ! "count"
-    readJSON _ = mzero 
--- instance JSON Address where
---     -- Keep the compiler quiet
---     showJSON = undefined
+data JSON = JSNull
+          | JSBool Bool
+          | JSNumber Int
+          | JSString String
+          | JSArray [JSON]
+          | JSObject [(String,JSON)]
+          deriving (Show, Eq)
 
---     readJSON (JSObject obj) =
---         Address        <$>
---         obj ! "house"  <*>
---         obj ! "street" <*>
---         obj ! "city"   <*>
---         obj ! "state"  <*>
---         obj ! "zip"
---     readJSON _ = mzero
+----------------------------------------------------------------
 
+json :: Parser JSON
+json = ws *> jsValue
 
--- instance JSON Person where
---     -- Keep the compiler quiet
---     showJSON = undefined
+jsValue :: Parser JSON
+jsValue = choice [jsNull,jsBool,jsObject,jsArray,jsNumber,jsString]
 
---     readJSON (JSObject obj) =
---         Person       <$>
---         obj ! "name" <*>
---         obj ! "age"  <*>
---         obj ! "address"
---     readJSON _ = mzero
+----------------------------------------------------------------
 
--- data Address = Address
---     { house  :: Integer
---     , street :: String
---     , city   :: String
---     , state  :: String
---     -- Renamed so as not to conflict with zip from Prelude
---     , zipC   :: Integer
---     } deriving (Show)
+-- |
+--
+-- >>> parseJSON "  null "
+-- Right JSNull
+jsNull :: Parser JSON
+jsNull = jsAtom "null" JSNull
 
--- data Person = Person
---     { name    :: String
---     , age     :: Integer
---     , address :: Address
---     } deriving (Show)
+-- |
+--
+-- >>> parseJSON "  false "
+-- Right (JSBool False)
+-- >>> parseJSON "true"
+-- Right (JSBool True)
+jsBool :: Parser JSON
+jsBool = jsAtom "false" (JSBool False)
+     <|> jsAtom "true"  (JSBool True)
 
--- aa :: String
--- aa = "{\"name\": \"some body\", \"age\" : 23, \"address\" : {\"house\" : 285, \"street\" : \"7th Ave.\", \"city\" : \"New York\", \"state\" : \"New York\", \"zip\" : 10001}}"
+----------------------------------------------------------------
 
-(!) :: (JSON a) => JSObject JSValue -> String -> Result a
-(!) = flip valFromObj
+-- |
+--
+-- >>> parseJSON "  { \"key1\": 2 ,  \"key2\" : false } "
+-- Right (JSObject [("key1",JSNumber 2),("key2",JSBool False)])
+jsObject :: Parser JSON
+jsObject = JSObject <$> betweenWs '{' kvs '}'
+  where
+    kvs = kv `sepBy` charWs ','
+    kv = do
+        JSString key <- jsString
+        charWs ':'
+        val <- jsValue
+        return (key, val)
 
+----------------------------------------------------------------
 
+-- |
+--
+-- >>> parseJSON "  [ 1 , \"foo\" ,  true ] "
+-- Right (JSArray [JSNumber 1,JSString "foo",JSBool True])
+jsArray :: Parser JSON
+jsArray = JSArray <$> betweenWs '[' vals ']'
+  where
+    vals = jsValue `sepBy` charWs ','
 
+----------------------------------------------------------------
 
--- main = print (decode aa :: Result Person)
+-- | Integer only.
+--
+-- >>> parseJSON "  123 "
+-- Right (JSNumber 123)
+-- >>> parseJSON "  -456 "
+-- Right (JSNumber (-456))
+jsNumber :: Parser JSON
+jsNumber = JSNumber <$> do
+    sign <- option id (negate <$ char '-')
+    ns   <- many1 $ oneOf ['0'..'9']
+    ws
+    return $ sign $ fromInts ns
+  where
+    fromInts = foldl' (\x y -> x*10 + toInt y) 0
+    toInt n = ord n - ord '0'
+
+----------------------------------------------------------------
+
+-- | Non Unicode only.
+--
+-- >>> parseJSON " \"foo bar baz\"  "
+-- Right (JSString "foo bar baz")
+jsString :: Parser JSON
+jsString = JSString <$> (between (char '"') (char '"') (many jsChar) <* ws)
+  where
+    jsChar = unescaped <|> escaped
+    unescaped = noneOf "\"\\"
+    escaped   = char '\\' *> escapedChar
+
+escapedChar :: Parser Char
+escapedChar = choice $ map ch alist
+  where
+    ch (x,y) = y <$ char x
+    alist = [
+        ('b', '\b')
+      , ('f', '\f')
+      , ('n', '\n')
+      , ('r', '\r')
+      , ('t', '\t')
+      , ('\\','\\')
+      , ('\"','\"')
+      ]
+
+----------------------------------------------------------------
+
+ws :: Parser ()
+ws = void $ many $ oneOf " \t\r\n"
+
+jsAtom :: String -> JSON -> Parser JSON
+jsAtom str val = val <$ (string str <* ws)
+
+charWs :: Char -> Parser ()
+charWs c = char c *> ws
+
+betweenWs :: Char -> Parser a -> Char -> Parser a
+betweenWs o vals c = charWs o *> vals <* charWs c
