38fb71730e4736afa05030d84a08783078d3fd8a
igorcj <cj.igor@outlook.com>
2021-02-05 10:44:25 -0300

    entity datatype

--- src/sintatic-entity-merge.hs
+++ src/sintatic-entity-merge.hs
@@ -3,37 +3,71 @@ import Data.Aeson
 import Data.Text
 import qualified Data.ByteString.Lazy as B
 
-data Person =
-  Person { firstName  :: !Text
-         , lastName   :: !Text
-         , age        :: Int
-         , likesPizza :: Bool
-           } deriving Show
+data Mention = Mention
+  { text :: String
+  , location :: [Int]
+  } deriving Show
 
-instance FromJSON Person where
- parseJSON (Object v) =
-    Person <$> v .: "firstName"
-           <*> v .: "lastName"
-           <*> v .: "age"
-           <*> v .: "likesPizza"
+newtype Disambiguation = Disambiguation
+  { subtypes :: [String]
+  } deriving Show
+
+data Entity = Entity
+  { entType :: String
+  , entText :: String
+  , mentions :: [Mention]
+  , disambiguation :: Disambiguation
+  , count :: Int
+  } deriving Show
+
+-- data Person =
+--   Person { firstName  :: !Text
+--          , lastName   :: !Text
+--          , age        :: Int
+--          , likesPizza :: Bool
+--            } deriving Show
+
+-- instance FromJSON Person where
+--  parseJSON (Object v) =
+--     Person <$> v .: "firstName"
+--            <*> v .: "lastName"
+--            <*> v .: "age"
+--            <*> v .: "likesPizza"
 --  parseJSON _ = mzero
 
-instance ToJSON Person where
- toJSON (Person firstName lastName age likesPizza) =
-    object [ "firstName"  .= firstName
-           , "lastName"   .= lastName
-           , "age"        .= age
-           , "likesPizza" .= likesPizza
-             ]
+instance FromJSON Entity where
+  parseJSON (Object v) =
+    Entity <$> v .: "type"
+           <*> v .: "text"
+           <*> v .: "mentions"
+           <*> v .: "disambiguation"
+           <*> v .: "count"
+
+-- instance ToJSON Person where
+--  toJSON (Person firstName lastName age likesPizza) =
+--     object [ "firstName"  .= firstName
+--            , "lastName"   .= lastName
+--            , "age"        .= age
+--            , "likesPizza" .= likesPizza
+--              ]
+
+instance ToJSON Entity where
+  toJSON (Entity entType entText mentions disambiguation count) =
+    object [ "type"           .= entType
+           , "text"           .= entText
+           , "mentions"       .= mentions
+           , "disambiguation" .= disambiguation
+           , "count"          .= count
+           ]
 
 
-jsonFile :: FilePath
-jsonFile = "trash.json"
+-- jsonFile :: FilePath
+-- jsonFile = "trash.json"
 
-getJSON :: IO B.ByteString
-getJSON = B.readFile jsonFile
+-- getJSON :: IO B.ByteString
+-- getJSON = B.readFile jsonFile
 
 
-main = do
-  content <- getJSON
-  print $ decode content
+-- main = do
+--   content <- getJSON
+--   print $ decode content
