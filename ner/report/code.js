12c75d041e23ad1f6ea729ca8646e3d54c122f3d
Alexandre Rademaker <arademaker@gmail.com>
2021-03-15 22:07:15 -0300

    testando ner, exemplo report HTML

---
+++ ner/report/code.js
@@ -0,0 +1,10 @@
+
+function load(){
+    document.getElementById('output').innerHTML = "Oh my God!";
+}
+
+function my() {
+    document.getElementById('output').innerHTML = '<ul><li>...<b>word</b>... </li><li>...<b>word</b>...</li></ul>';
+}
+
+
