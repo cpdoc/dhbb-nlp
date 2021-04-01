54eb3c43865fb27bfdf9481484aa148ea30c0b9a
Alexandre Rademaker <arademaker@gmail.com>
2021-04-01 19:34:55 -0300

    dicionarios

--- ner/report/code.js
+++ ner/report/code.js
@@ -1,10 +1,16 @@
 
 function load(){
-    document.getElementById('output').innerHTML = "Oh my God!";
+    document.getElementById('examples').innerHTML = "Oh my God!";
+    create_table();
 }
 
-function my() {
-    document.getElementById('output').innerHTML = '<ul><li>...<b>word</b>... </li><li>...<b>word</b>...</li></ul>';
+function my(obj) {
+    document.getElementById('examples').innerHTML = '<p>' + obj.id +  '</p><ul><li>...<b>word</b>... </li><li>...<b>word</b>...</li></ul>';
+}
+
+function create_table() {
+    // ler o json
+    document.getElementById('table').innerHTML = '<table style="border: 1px solid black;"> <tr> <td><span id="cell11" onclick="my(this)">12</span></td><td>12</td> </tr><tr> <td>12</td><td>12</td> </tr> </table>';
 }
 
 
