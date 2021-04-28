69a6b96a65d47d7f74b7cf0e721ec77a458d6bf9
Alexandre Rademaker <arademaker@gmail.com>
2021-04-28 13:51:10 -0300

    code lisp morpho vs conllu

---
+++ src/morphobr.lisp
@@ -0,0 +1,93 @@
+
+(ql:quickload '(:cl-ppcre :alexandria :cl-conllu))
+
+(in-package :conllu.user)
+
+(defstruct entry form lemma pos lpat)
+
+(defun line-to-entry (line)
+  (destructuring-bind (form rest)
+      (cl-ppcre:split "\\t" (string-trim '(#\Space #\Tab) line))
+    (let ((as (cl-ppcre:split "\\+" rest)))
+      (make-entry :form form :lemma (car as) :pos (cadr as)
+		  :lpat (format nil "~{~a~^+~}" (subseq as 1))))))
+
+
+(defun read-dict (file dict)
+  (with-open-file (in file)
+    (loop for line = (read-line in nil nil)
+	  while line
+	  do (let ((e (line-to-entry line)))
+	       (push e (gethash (entry-form e) dict)))
+	  finally (return dict))))
+
+(defun read-morphobr ()
+  (let ((dicts (append (directory "/Users/ar/work/morpho-br/nouns/*.dict")
+		       (directory "/Users/ar/work/morpho-br/adjectives/*.dict")
+		       (directory "/Users/ar/work/morpho-br/verbs/*.dict")
+		       (directory "/Users/ar/work/morpho-br/adverbs/*.dict"))))
+    (reduce (lambda (tb file) (read-dict file tb)) dicts 
+	    :initial-value (make-hash-table :test #'equal))))
+
+
+(defun read-mapping (fn)
+  (with-open-file (in fn)
+    (loop for line = (read-line in nil nil)
+	  while line
+	  for reg = (cl-ppcre:split "[ ]+" (string-trim '(#\Space #\Tab) line))
+	  when (> (length reg) 3) collect (subseq reg 1))))
+
+
+
+(defun unique? (lst)
+  (equal 1(length lst)))
+
+
+(defun filter-map (tk mapping)
+  (remove-if-not (lambda (e)
+		   (and (equal (token-feats tk) (cadr e))
+			(equal (token-upostag tk) (car e))))
+		 mapping))
+
+(defun filter-dic (entries mapping)
+  (remove-if-not (lambda (e)
+		   (member (entry-lpat e) mapping :test #'equal :key #'caddr))
+		 entries))
+
+
+(defun equal-pos (pdict pud)
+  (let ((map '(("NOUN" . "N") ("VERB" . "V") ("ADJ" . "A") ("ADV" . "ADV"))))
+    (equal pdict (cdr (assoc pud map :test #'equal)))))
+
+(defun compatible (tk entry)
+  (and (equal     (entry-lemma entry) (token-lemma tk))
+       (equal-pos (entry-pos entry)   (token-upostag tk))
+       (equal     (entry-form entry)  (string-downcase (token-form tk)))))
+
+(defun check (tk dict mapping stream)
+  (if (member (token-upostag tk) (list "VERB" "ADV" "NOUN" "ADJ" "AUX") :test #'equal)
+      (let* ((lform   (string-downcase (token-form tk)))
+	     (entries0 (gethash lform dict))
+	     (entries1 (remove-if-not (lambda (e) (compatible tk e)) entries0)))
+	(cond
+	  ((equal 1 (length entries1))
+	   ;; (format stream "~a ok~%" tk)
+	   )
+	  ((> (length entries1) 1)
+	   (format stream "~a ambiguos, found ~a~%" tk entries1))
+	  ((not entries1)
+	   (format stream "~a not found in dict~%" tk))))))
+
+
+(defparameter mapping (read-mapping "mapping.txt"))
+(defparameter dic (read-morphobr))
+
+(defun main (dict map)
+  (mapc (lambda (fn)
+	  (loop for s in (cl-conllu:read-conllu fn)
+		do (progn
+		     (format *standard-output* "Sentece: ~a~%" (sentence-id s))
+		     (loop for tk in (sentence-tokens s)
+			   do (check tk dict map *standard-output*)))))
+	(directory "../udp-mini/*.conllu")))
+
